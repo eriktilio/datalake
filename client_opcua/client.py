5cbd2ed33944de86506411235049beb2fbc06ae8
erik.rey <erik.rey@itriad.org.br>
2024-02-10 12:00:29 -0400

    feat: adding coments and added mysql_database

--- client_opcua/client.py
+++ client_opcua/client.py
@@ -11,24 +11,29 @@ class OPCUA_Client:
         self.client = None
 
     async def __aenter__(self):
+        # Criação do cliente OPC UA e conexão ao servidor.
         self.client = Client(url=self.server_url)
         await self.client.connect()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
+        # Desconexão do cliente OPC UA ao sair do contexto.
         await self.disconnect()
 
     async def read_variable(self, variable_path):
+        # Leitura do valor de uma variável no servidor OPC UA.
         variable_node = self.client.get_node(variable_path)
         value = await variable_node.read_value()
         return value
 
     async def disconnect(self):
+        # Desconexão do cliente OPC UA.
         if self.client:
             await self.client.disconnect()
 
 
 async def send_to_logstash(value, timestamp):
+    # Montagem dos dados no formato desejado e envio para o Logstash via HTTP.
     json_data = {
         "pressao": float(value),
         "timestamp": timestamp.strftime("%Y-%m-%d %H:%M:%S"),
@@ -43,24 +48,27 @@ async def send_to_logstash(value, timestamp):
 
 
 async def main():
+    # Configurações iniciais, incluindo o URL do servidor OPC UA e o caminho da variável.
     server_url = "opc.tcp://opc_data_generator:4840/freeopcua/server/"
     variable_path = "ns=2;s=freeopcua.Tags.pressure"
 
     async with OPCUA_Client(server_url) as opcua_client:
         try:
             while True:
+                # Leitura periódica do valor da variável OPC UA.
                 value = await opcua_client.read_variable(variable_path)
                 print(f"Valor da variável de pressão: {value}")
 
-                # Envie o valor para o Logstash a cada leitura
+                # Envio do valor lido para o Logstash.
                 timestamp = datetime.now()
                 await send_to_logstash(value, timestamp)
 
-                # Aguarde um intervalo (por exemplo, 5 segundos) antes de ler novamente
+                # Aguardo de um intervalo (por exemplo, 10 segundos) antes de ler novamente.
                 await asyncio.sleep(10)
         except KeyboardInterrupt:
             print("Script interrompido manualmente. Encerrando o loop.")
 
 
 if __name__ == "__main__":
+    # Execução da função principal utilizando o asyncio.
     asyncio.run(main())
--- opc_mock/opc_data_generator.py
+++ opc_mock/opc_data_generator.py
@@ -6,13 +6,15 @@ from asyncua import Server, ua
 
 
 async def run_opcue():
+    # Configuração do logger para o módulo asyncua.
     _logger = logging.getLogger("asyncua")
-    # Configurar nosso servidor
+
+    # Configuração do servidor OPC UA.
     server = Server()
     await server.init()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
 
-    # Configurar nosso próprio namespace, não é realmente necessário, mas recomendado
+    # Configuração do namespace.
     uri = "http://examples.freeopcua.github.io"
     idx = await server.register_namespace(uri)
     ns = "ns=2;s=freeopcua.Tags.pressure"
@@ -20,29 +22,31 @@ async def run_opcue():
     min_val = -0.5
     max_val = 0.6
 
-    # Preencher nosso espaço de endereçamento
-    # server.nodes contém links para nós muito comuns como objetos e raiz
+    # Preenchimento do espaço de endereçamento.
     my_object = await server.nodes.objects.add_object(idx, "my_object")
     pressure = await my_object.add_variable(ns, "pressure", 10.5)
-    # Definir MinhaVariavel como gravável pelos clientes
+
+    # Configuração da variável como gravável pelos clientes.
     await pressure.set_writable()
     opcs = [pressure]
 
-    _logger.info("Iniciando o servidor!")
+    _logger.info("Iniciando o servidor OPC UA!")
+
+    # Inicialização do servidor OPC UA e execução do loop principal.
     async with server:
         while True:
             await asyncio.sleep(1)
+            # Simulação de valores de pressão aleatórios.
             counter = random.uniform(min_val, max_val)
             for opc in opcs:
                 new_value = await opc.get_value() + counter
-                if new_value > 100.0:
-                    new_value = 100.0
-                elif new_value < 0.0:
-                    new_value = 0.0
+                # Limitação dos valores entre 0.0 e 100.0.
+                new_value = min(100.0, max(0.0, new_value))
                 _logger.info("Definir valor de %s para %.1f", opc, new_value)
                 await opc.write_value(new_value)
 
 
 if __name__ == "__main__":
+    # Configuração do logger principal e execução da função principal utilizando o asyncio.
     logging.basicConfig(level=logging.DEBUG)
     asyncio.run(run_opcue(), debug=True)
