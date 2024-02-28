08ba8139e5e45516c67c6a0a2e328ac615517e12
erik.rey <erik.rey@itriad.org.br>
2024-02-28 11:09:27 -0400

    refactor: adjustments on services execution

--- client_opcua/client.py
+++ client_opcua/client.py
@@ -1,7 +1,8 @@
 import asyncio
-from datetime import datetime
-
 import requests
+import os
+
+from datetime import datetime
 from asyncua import Client
 
 
@@ -11,36 +12,32 @@ class OPCUA_Client:
         self.client = None
 
     async def __aenter__(self):
-        # Criação do cliente OPC UA e conexão ao servidor.
         self.client = Client(url=self.server_url)
         await self.client.connect()
         return self
 
     async def __aexit__(self, exc_type, exc, tb):
-        # Desconexão do cliente OPC UA ao sair do contexto.
         await self.disconnect()
 
     async def read_variable(self, variable_path):
-        # Leitura do valor de uma variável no servidor OPC UA.
         variable_node = self.client.get_node(variable_path)
         value = await variable_node.read_value()
         return value
 
     async def disconnect(self):
-        # Desconexão do cliente OPC UA.
         if self.client:
             await self.client.disconnect()
 
 
 async def send_to_logstash(value, timestamp):
-    # Montagem dos dados no formato desejado e envio para o Logstash via HTTP.
+    logstash_url = os.environ.get("LOGSTASH_URL", "http://logstash:8080")
     json_data = {
         "pressao": float(value),
         "timestamp": timestamp.strftime("%Y-%m-%d %H:%M:%S"),
     }
 
     try:
-        response = requests.post("http://logstash:8080", json=json_data)
+        response = requests.post(logstash_url, json=json_data)
         response.raise_for_status()
         print("Dados enviados com sucesso para o Logstash.")
     except requests.exceptions.RequestException as e:
@@ -48,27 +45,25 @@ async def send_to_logstash(value, timestamp):
 
 
 async def main():
-    # Configurações iniciais, incluindo o URL do servidor OPC UA e o caminho da variável.
-    server_url = "opc.tcp://opc_data_generator:4840/freeopcua/server/"
+    opc_url = os.environ.get("OPC_DATA_URL", "opc.tcp://opc_data_generator:4840")
+    server_url = opc_url + "/freeopcua/server/"
     variable_path = "ns=2;s=freeopcua.Tags.pressure"
 
     async with OPCUA_Client(server_url) as opcua_client:
         try:
             while True:
-                # Leitura periódica do valor da variável OPC UA.
                 value = await opcua_client.read_variable(variable_path)
                 print(f"Valor da variável de pressão: {value}")
 
-                # Envio do valor lido para o Logstash.
+                # Envie o valor para o Logstash a cada leitura
                 timestamp = datetime.now()
                 await send_to_logstash(value, timestamp)
 
-                # Aguardo de um intervalo (por exemplo, 10 segundos) antes de ler novamente.
+                # Aguarde um intervalo (por exemplo, 5 segundos) antes de ler novamente
                 await asyncio.sleep(10)
         except KeyboardInterrupt:
             print("Script interrompido manualmente. Encerrando o loop.")
 
 
 if __name__ == "__main__":
-    # Execução da função principal utilizando o asyncio.
     asyncio.run(main())
