ea67f4ad7cb7c70152fae702b4225135cdefb4db
erik.rey <erik.rey@itriad.org.br>
2024-03-09 18:16:20 -0400

    feat: up only logstash and elk

--- mysql_database/migrations/populate_table.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO produtos (nome, descricao, preco) VALUES
-    ('Produto A', 'Descrição do Produto A', 29.99),
-    ('Produto B', 'Descrição do Produto B', 49.99),
-    ('Produto C', 'Descrição do Produto C', 79.99);
--- mysql_database/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Construir a imagem Docker
-docker build -t my-database-mysql .
-
-# Executar o container com o Flyway
-docker run -p 3306:3306 my-database-mysql
